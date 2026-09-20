34248dcc7c16ac9fcc3ea38d6a23ba9fd1212a52
chayoungdal <103127850+chayoungdal@users.noreply.github.com>
2026-09-20 23:49:01 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'my-life-v22';
+const CACHE_NAME = 'my-life-v23';
 const ASSETS = ['./index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
